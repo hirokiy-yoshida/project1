6ffcceb6a4fba81a8e78932201639b24622c89c1
hiroki yoshida <yoshida-hiroki@corollaakita.jp>
2025-04-25 16:16:45 +0900

    Initial commit

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
-const { createServer } = require('https');
-const { parse } = require('url');
-const next = require('next');
-const fs = require('fs');
-const path = require('path');
+const { createServer } = require("https");
+const { parse } = require("url");
+const next = require("next");
+const fs = require("fs");
+const path = require("path");
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const httpsOptions = {
-  key: fs.readFileSync(path.join(process.cwd(), 'certificates', 'localhost-key.pem')),
-  cert: fs.readFileSync(path.join(process.cwd(), 'certificates', 'localhost.pem')),
-};
+const port = process.env.PORT || 3000;
 
 app.prepare().then(() => {
-  createServer(httpsOptions, (req, res) => {
+  const server = createServer((req, res) => {
     // セキュリティヘッダーの追加
-    res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
-    res.setHeader('X-Content-Type-Options', 'nosniff');
-    res.setHeader('X-Frame-Options', 'DENY');
-    res.setHeader('X-XSS-Protection', '1; mode=block');
-    
+    res.setHeader(
+      "Strict-Transport-Security",
+      "max-age=31536000; includeSubDomains"
+    );
+    res.setHeader("X-Content-Type-Options", "nosniff");
+    res.setHeader("X-Frame-Options", "DENY");
+    res.setHeader("X-XSS-Protection", "1; mode=block");
+
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(3000, (err) => {
+  });
+
+  server.listen(port, (err) => {
     if (err) throw err;
-    console.log('> Ready on https://localhost:3000');
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
